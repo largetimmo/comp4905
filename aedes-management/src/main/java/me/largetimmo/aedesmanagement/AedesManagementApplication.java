fc0aaef9113c82b65f45293487abda9c89e2f730
JUNHAO CHEN <kylechen49@gmail.com>
2020-09-21 12:50:14 -0400

    Init management module

---
+++ aedes-management/src/main/java/me/largetimmo/aedesmanagement/AedesManagementApplication.java
@@ -0,0 +1,13 @@
+package me.largetimmo.aedesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AedesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AedesManagementApplication.class, args);
+	}
+
+}
---
+++ aedes-management/src/test/java/me/largetimmo/aedesmanagement/AedesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package me.largetimmo.aedesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AedesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
